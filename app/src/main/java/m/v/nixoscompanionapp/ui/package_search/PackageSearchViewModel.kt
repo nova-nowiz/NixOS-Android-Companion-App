40d94e56c692ef2fd71402ccae1e8a3b90dde098
Narice <narice@tutanota.com>
2021-03-18 23:44:30 +0100

    Changed to volley jsonObjectRequest
    I can now get the items from the package search which is ultra good

--- app/src/main/java/m/v/nixoscompanionapp/ui/package_search/PackageSearchViewModel.kt
+++ app/src/main/java/m/v/nixoscompanionapp/ui/package_search/PackageSearchViewModel.kt
@@ -1,10 +1,13 @@
 package m.v.nixoscompanionapp.ui.package_search
 
+import android.app.Application
+import android.content.Context
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -17,7 +20,7 @@ import java.net.MalformedURLException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
-class PackageSearchViewModel : ViewModel() {
+class PackageSearchViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is the package search Fragment"
@@ -26,51 +29,47 @@ class PackageSearchViewModel : ViewModel() {
 
     fun makeApiRequest(jsonBody: String) {
         viewModelScope.launch {
-            val bearerAuth = "Bearer ejNaRko2eTJtUjpkczhDRXZBTFBmOXB1aTdYRw=="
-            val response = makeNixOSElasticSearchRequest(ElasticSearchType.Package, jsonBody, bearerAuth)
-            _text.value = response?.toString(2)
+            VolleySingleton
+                .getInstance(getApplication<Application>().baseContext)
+                .addToRequestQueue(
+                    object : JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://nixos-search-5886075189.us-east-1.bonsaisearch.net/latest-19-20.09/_search",
+                        JSONObject(jsonBody),
+                        { response: JSONObject ->
+                            _text.value = response.toString(2)
+                        },
+                        null
+                    ) {
+                        override fun getHeaders(): MutableMap<String, String> {
+                            val headers = HashMap<String, String>(super.getHeaders())
+                            headers["Authorization"] = "Bearer ejNaRko2eTJtUjpkczhDRXZBTFBmOXB1aTdYRw=="
+                            headers["Content-Type"] = "application/json"
+                            return headers
+                        }
+                    }
+                )
         }
     }
 }
 
-enum class ElasticSearchType {
-    Package,
-    Option
-}
-
-suspend fun makeNixOSElasticSearchRequest(searchType: ElasticSearchType, jsonBody: String, auth: String): JSONObject? =
-    withContext(Dispatchers.IO) {
-        try {
-            val url = URL("https://nixos-search-5886075189.us-east-1.bonsaisearch.net/latest-19-20.09/_search")
-            (url.openConnection() as? HttpsURLConnection)?.run {
-                requestMethod = "GET"
-                setRequestProperty("Content-Type", "application/json")
-                setRequestProperty("Authorization", auth)
-                //doOutput = true
-                //outputStream.write(jsonBody.toByteArray())
-                val stream = BufferedInputStream(inputStream)
-                val result: String = readStream(stream)
-                Log.i("NixOS Package Search", result)
-                return@withContext JSONObject(result)
+class VolleySingleton constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: VolleySingleton? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: VolleySingleton(context).also {
+                    INSTANCE = it
+                }
             }
-        } catch (e: MalformedURLException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return@withContext null
     }
-
-private fun readStream(inputStream: InputStream): String {
-    return try {
-        val bo = ByteArrayOutputStream()
-        var i = inputStream.read()
-        while (i != -1) {
-            bo.write(i)
-            i = inputStream.read()
-        }
-        bo.toString()
-    } catch (e: IOException) {
-        ""
+    val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
     }
 }
